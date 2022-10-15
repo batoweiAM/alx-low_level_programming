6e405af93e412c057c283da8b8c262581c0c99ae
batoweiAM <batoweimike94@gmail.com>
2022-10-14 17:19:40 -0700

    Six print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: 0 no error, non-zero, if error.
+ * */
+int main(void)
+{
+	int a;
+
+	for (a ='0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
