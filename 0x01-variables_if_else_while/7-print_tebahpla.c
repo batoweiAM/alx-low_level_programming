cf8521ea32bfe9eab785e70187efb5b6f903f339
batoweiAM <batoweimike94@gmail.com>
2022-10-14 17:30:25 -0700

    Seven print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 if no errors, non-zero if errors.
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
