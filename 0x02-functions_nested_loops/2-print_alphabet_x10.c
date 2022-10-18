abd59ccbebaa665041a23f8b1092df21df4a3c24
batoweiAM <batoweimike94@gmail.com>
2022-10-18 14:03:19 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all alphabet in lowercase 10 times.
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i = 0. j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 26)
+		{
+			_putchar(j = 'a'; j <= 'z';);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
