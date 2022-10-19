1ca61ba96c62899fe3d09de5f003f379e07abb60
batoweiAM <batoweimike94@gmail.com>
2022-10-19 15:00:07 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,32 +2,31 @@
 /**
  *times_table - prints the 9 times table, starting with 0.
  *
- *Return: always 0 success.
+ *Return: no return.
 */
 void times_table(void)
 {
-	int a, b, mn;
+	int a, b, op;
 
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
 		for (b = 1; b <= 9; b++)
 		{
-			mn = a * b;
+			op = a * b;
 			_putchar(44);
 			_putchar(32);
-			if (mn <= 9)
+			if (op <= 9)
 			{
 				_putchar(32);
-				_putchar(mn + 48);
+				_putchar(op + 48);
 			}
 			else
 			{
-				_putchar((mn / 10) + 48);
-				_putchar((mn % 10) + 48);
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
 			}
 		}
 		_putchar('\n');
-		return (0);
 	}
 }
